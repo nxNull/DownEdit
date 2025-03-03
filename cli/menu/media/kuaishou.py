9ff24960a928f76a3a22ca34fa76d4e469551737
sokunheng <44894784+sokunheng@users.noreply.github.com>
2025-03-03 22:15:12 +0700

    Update

--- cli/menu/media/kuaishou.py
+++ cli/menu/media/kuaishou.py
@@ -14,10 +14,15 @@ def main():
         banner_display,
         banner_msg
     )
+
     user = input(f"{Fore.YELLOW}Enter User Link:{Fore.WHITE} ")
     # Is Working with cookies is a bit tricky. Currently we'll just ask the user to input it
     cookies = input(f"{Fore.YELLOW}Enter Cookies:{Fore.WHITE} ")
-    kuaishou = KuaiShou(user=user, cookies=cookies)
+
+    kuaishou = KuaiShou(
+        user=user,
+        cookies=cookies
+    )
     kuaishou.download_all_videos()
 
 if __name__ == "__main__":
--- cli/menu/media/youtube.py
+++ cli/menu/media/youtube.py
@@ -14,6 +14,7 @@ def main():
         banner_display,
         banner_msg
     )
+
     channel = input(f"{Fore.YELLOW}Enter Channel Url:{Fore.WHITE} ")
     video_type = selector.select_menu(
         message=f"{Fore.YELLOW}Select Video Type:{Fore.WHITE}",
@@ -22,7 +23,11 @@ def main():
             " Shorts": "shorts"
         }
     )
-    youtube = Youtube(channel, video_type=video_type.lower().lstrip())
+
+    youtube = Youtube(
+        channel=channel,
+        video_type=video_type.lower().lstrip()
+    )
     youtube.download_all_videos()
 
 if __name__ == "__main__":
